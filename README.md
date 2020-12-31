d5bcf974ad825fbda54555fa909ae5d397508a7e
Eupólemo Castro <eupolemo@gmail.com>
2020-12-30 21:58:21 -0300

    Issue #1 Update chat message with l5r roll; Add .gitattributes; Add example to readme; Correct 1 rise caused formula error;

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fvtt-l5r4ed-dice-roller
 An implementation of a dice roller to Legends of the five rings 4th Edition game system for Foundry Virtual Tabletop (http://foundryvtt.com).
+
+## Roll
+It captures chat message and converts to a Foundry VTT roll pattern and shows the result with the Legend of Five Rings text roll.
+
+Example: 10k6 is converted to '/r 10d10k6x10'
+
+<img src="readme-resources/roll-l5r.gif"/>
--- dice-roller.js
+++ dice-roller.js
@@ -1,16 +1,43 @@
 Hooks.on("chatMessage", function (chatlog, message, chatdata) {
-  var oldMessage = message;
   let pattern = /^\d+k\d+([+]\d+)?$/;
 
-  console.log(`Parser to message ${message} ${pattern.test(message)}`)
-
-  if(pattern.test(message)) {
+  if ( pattern.test(message) ) {
     message = roll_parser(message)
+    roll_parsed = message
 
-    chatlog.processMessage(message)
+    chatlog.processMessage(`/r ${message}`)
     return false
   }
 });
+
+Hooks.on('renderChatMessage', async (app, html, msg) => {
+  if ( !app.isRoll || !app.isContentVisible ) return;
+  let pattern = /^\d+d\d+k\d+(x\d+)?( \+\ \d+)?$/;
+
+  const roll = app.roll
+  const formula = roll.formula
+  const die = roll.dice[0]
+  let bonus = 0
+
+  if ( roll.terms.includes('+') ) {
+    bonus = roll.terms[roll.terms.indexOf('+') + 1]
+  }
+
+  if ( pattern.test(formula) ) {
+    const b_div_tag = '<div class="dice-formula">'
+    const e_div_tag = "</div>"
+    const b_span_tag = '<span class="part-formula">'
+    const e_span_tag = '</span>'
+    const regex_div = new RegExp(`${b_div_tag}.*?${e_div_tag}`, 'g')
+    const regex_span = new RegExp(`${b_span_tag}.*?${e_span_tag}`, 'g')
+
+    let roll_l5r = `${die.number}${die.modifiers[0]}${bonus > 0 ? ' + ' + bonus : ''}${die.modifiers.length > 1 ? ' Exploding: ' + die.modifiers[1].replace('x', '') : ''}`
+
+    msg.message.content = msg.message.content.replace(regex_div, `${b_div_tag} ${roll_l5r} ${e_div_tag}`).replace(regex_span, `${b_span_tag} ${roll_l5r} ${e_span_tag}`);
+    html.find(".dice-formula")[0].innerHTML = roll_l5r
+    html.find(".part-formula")[0].innerHTML = roll_l5r
+  }
+})
 
 function roll_parser(roll) {
   let [dices, kept_plus_bonus] = roll.split`k`.map(parseIntIfPossible);
@@ -23,16 +50,14 @@ function roll_parser(roll) {
     rises: 0,
     explode: 10
   }
-  console.log(roll_values)
   let result = calculate_roll(roll_values)
-  console.log(result)
-  return `/r ${result.dices}d10k${result.kept}x${result.explode}+${result.bonus}`
-  // return `/r ${result.dices}d10x${result.explode}${result.dices - result.kept <= 0 ? '' : 'dl' + (10 - result.kept)}+${result.bonus}`
+  roll_l5r = `${result.dices}k${result.kept}${result.bonus > 0 ? ' + ' + result.bonus : result.bonus < 0 ? ' - ' + result.bonus : ''}${result.explode <= 10 ? ' Exploding ' + result.explode : ''}`
+  return `${result.dices}d10k${result.kept}x${result.explode}+${result.bonus}`
 }
 
 function parseIntIfPossible(x) {
   var numbers = /^[0-9]+$/;
-  if(x.match(numbers)){
+  if ( x.match(numbers) ) {
     return parseInt(x)
   } else {
     return x
@@ -40,15 +65,15 @@ function parseIntIfPossible(x) {
 }
 
 function calculate_roll(roll) {
-  let result = roll;
+  let calculated_roll = roll;
   ({dices, rises} = calculate_rises(roll));
-  result.dices = dices;
-  result.rises = rises;
-  ({kept, rises} = calculate_keeps(result));
-  result.rises = rises;
-  result.kept = kept;
-  result.bonus = calculate_bonus(result);
-  return result
+  calculated_roll.dices = dices;
+  calculated_roll.rises = rises;
+  ({kept, rises} = calculate_keeps(calculated_roll));
+  calculated_roll.rises = rises;
+  calculated_roll.kept = kept;
+  calculated_roll.bonus = calculate_bonus(calculated_roll);
+  return calculated_roll
 };
 
 function calculate_rises({dices, rises} = roll) {
@@ -60,16 +85,15 @@ function calculate_rises({dices, rises} = roll) {
 }
 
 function calculate_keeps({kept, rises} = roll) {
-  if(kept >=10) {
+  if ( kept >=10 ) {
     rises += kept - 10;
     kept = 10;
   }
 
-  while(kept < 10) {
-    if(rises > 0) {
-      if(rises > 1)
-        kept++;
-        rises -= 2;
+  while ( kept < 10 ) {
+    if ( rises > 1 ) {
+      kept++;
+      rises -= 2;
     } else {
       break;
     }
